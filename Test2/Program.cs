77cc8722e4b8c61b55661bbcdc46c762d521f521
Lennart0 <lennart0@hotmail.se>
2016-04-29 15:50:18 +0200

    Update Program.cs
    Changed to bitbucket

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -9,7 +9,7 @@ namespace Test2
     class Program
     {
         static void Main( string[] args ) {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello Bitbucket!");
         }
     }
 }
